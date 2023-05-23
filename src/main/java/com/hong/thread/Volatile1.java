e8c7272cfbc8e44db40e68af381080b73b35db34
hongjinfeng <m13075826790@163.com>
2023-05-23 23:07:57 +0800

    2023-05-23 学学学

---
+++ src/main/java/com/hong/thread/Volatile1.java
@@ -0,0 +1,44 @@
+package com.hong.thread;
+
+import java.io.File;
+
+/**
+ * @Author hong
+ * @Date 2023/5/23 21:57
+ */
+public class Volatile1 {
+
+    static int i = 0;
+
+    public static void main(String[] args) {
+
+        new Thread(() -> {
+            while (true) {
+//                System.out.println("haha");
+//                File file = new File("");
+                if (i == 1) {
+                    System.out.println(Thread.currentThread().getName() + ">>>>> i = " + i);
+                    break;
+                }
+//                file.canExecute();
+//                synchronized (Volatile1.class) {
+//                }
+//                try {
+//                    Thread.sleep(1000);
+//                } catch (InterruptedException e) {
+//                    throw new RuntimeException(e);
+//                }
+            }
+        }, "A").start();
+
+        new Thread(() -> {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            i = 1;
+        }, "B").start();
+
+    }
+}
---
+++ src/main/java/com/hong/thread/Volatile2.java
@@ -0,0 +1,41 @@
+package com.hong.thread;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author hong
+ * @Date 2023/5/23 22:50
+ */
+public class Volatile2 {
+
+    static int i = 0;
+    private static final int count = 600; // 100 200 300 ... 10000 100000
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            final List<Integer> list = new ArrayList<>();
+            for (int j = 1; j <= count; j++) {
+                list.add(j);
+            }
+
+            while (true) {
+                if (list.contains(i)) {
+                    System.out.println(Thread.currentThread().getName() + ">>>> i = " + i);
+                    break;
+                }
+            }
+        }, "A").start();
+
+        new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            for (int j = 1; j <= count; j++) {
+                i = j;
+            }
+        }, "B").start();
+    }
+}
