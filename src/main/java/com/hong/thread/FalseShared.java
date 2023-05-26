b01678e3391f02b311ca677e3d51a164e88209a3
hongjinfeng <m13075826790@163.com>
2023-05-26 20:51:32 +0800

    2023-05-26 学学学

---
+++ src/main/java/com/hong/thread/FalseShared.java
@@ -0,0 +1,38 @@
+package com.hong.thread;
+
+/**
+ * @Author hong
+ * @Date 2023/5/26 19:44
+ */
+public class FalseShared {
+
+    public static void main(String[] args) throws InterruptedException {
+        final long start = System.currentTimeMillis();
+        final Count count = new Count();
+        final Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 100000000; i++) {
+                count.a++;
+            }
+        });
+
+        final Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 100000000; i++) {
+                count.b++;
+            }
+        });
+
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        // 3332 3562 3759
+        System.out.println("item:" + (System.currentTimeMillis() - start));
+    }
+}
+
+class Count {
+    volatile long a;
+//    public long p1,p2,p3,p4,p5,p6,p7;
+    @jdk.internal.vm.annotation.Contended
+    volatile long b;
+}
